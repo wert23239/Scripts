3b3b01837b10f102b4b6f0d954525cc00cbf43c6
wert23239 <lambe168@msu.edu>
2016-08-05 14:35:22 -0400

    Redid Some Files and Added New Scripts

---
+++ FindHighest.bat
@@ -0,0 +1,5 @@
+cd C:/Users/Alex/Desktop
+scp -r lambe168@rsync.hpcc.msu.edu:~/Jobs/MoveForAug .
+pause
+python FindHighestValue.py
+pause
--- Master.bat
+++ Master.bat
@@ -1,3 +1,8 @@
 call ZipFile.bat
+pause
+call Localize.bat
 call CopyToHPCC.bat
+pause
+call Localize.bat
 call SSHandCall.bat
+pause
