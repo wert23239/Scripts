135ae232e5e24dda14792cca101f72f7f52e0cb6
wert23239 <lambe168@msu.edu>
2016-08-09 22:19:53 -0400

    Added Binary Test Driven Development
    Added A file to run a batch script thru reasearch
    Still very wet code

---
+++ Test01.bat
@@ -0,0 +1,14 @@
+cd C:/Source/MABE/Results
+( 
+echo 1 1 0 0
+echo 1 1 0 0 
+echo 1 1 0 0
+echo 1 1 0 0
+pause
+pause
+pause
+echo 1 1 0 0
+echo 1 1 0 0
+echo 1 1 0 0
+echo 1 1 0 0
+) | MABE.exe -f settings.cfg settings_organism.cfg settings_world.cfg
