feb24891d21997d370d438ee1989340543c523f2
wert23239 <lambe168@msu.edu>
2016-08-09 23:16:25 -0400

    Created For Loop for input file
    But it really doesn't work :/

--- Test01.bat
+++ Test01.bat
@@ -1,14 +1,13 @@
+@echo off
 cd C:/Source/MABE/Results
+setlocal enableDelayedExpansion
 ( 
 echo 1 1 0 0
-echo 1 1 0 0 
 echo 1 1 0 0
+FOR /l %%x in (1, 1, 17) DO (
 echo 1 1 0 0
+)
+echo File Not Found
 pause
-pause
-pause
-echo 1 1 0 0
-echo 1 1 0 0
-echo 1 1 0 0
-echo 1 1 0 0
-) | MABE.exe -f settings.cfg settings_organism.cfg settings_world.cfg
+) |MABE.exe -f settings.cfg settings_organism.cfg settings_world.cfg
+
