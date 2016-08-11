b2b38bbcfb390aea8f6f1a368e1bd720dc9d6426
wert23239 <lambe168@msu.edu>
2016-08-10 20:05:48 -0400

    Fix it's

--- Test01BAK.bat
+++ Test01BAK.bat
@@ -8,28 +8,23 @@ echo 1 1 0 0
 FOR /l %%x in (1, 1, 1) DO (
 FOR /l %%w in (1, 1, 10) DO (
 echo 1 1 0 0
-echo 1 1 0 0)) 
-::Move Both to Middle but top is one Above Middle
+echo 1 1 0 0)) ::Move Both to Middle but top is one Above Middle
 FOR /l %%x in (1, 1, 1) DO (
 FOR /l %%w in (1, 1, 1) DO (
 echo 0 1 0 0
-echo 0 0 0 0)) 
-::Rotate Bottom One Right
+echo 0 0 0 0)) ::Rotate Bottom One Right
 FOR /l %%x in (1, 1, 1) DO (
 FOR /l %%w in (1, 1, 1) DO (
 echo 1 1 0 0
-echo 1 1 0 0)) 
-::Move Bottom One Right and Top Down
+echo 1 1 0 0)) ::Move Bottom One Right and Top Down
 FOR /l %%x in (1, 1, 1) DO (
 FOR /l %%w in (1, 1, 1) DO (
 echo 0 0 0 0
-echo 0 1 0 0)) 
-::Rotate Top One Left
+echo 0 1 0 0)) ::Rotate Top One Left
 FOR /l %%x in (1, 1, 1) DO (
 FOR /l %%w in (1, 1, 10) DO (
 echo 1 1 0 0
-echo 1 1 0 0)) 
-::Move Bottom to Edge
+echo 1 1 0 0)) ::Move Bottom to Edge
 pause
-) | MABE.exe -f settings.cfg settings_organism.cfg settings_world.cfg 
+) |MABE.exe -f settings.cfg settings_organism.cfg settings_world.cfg 
 
